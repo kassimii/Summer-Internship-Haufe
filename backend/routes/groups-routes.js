859286a5ed120bca0a701164735370625969c306
kassimii <patyboc@yahoo.com>
2020-07-26 23:50:47 +0300

    Merge update

--- backend/routes/groups-routes.js
+++ backend/routes/groups-routes.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const {
-    getGroups,
-    getGroupsById,
-    updateGroup
-} = require('../controllers/groups-controller');
+  createGroup,
+  deleteGroup,
+  getGroups,
+  getGroupsById,
+  updateGroup,
+} = require("../controllers/groups-controller");
 
-router.get('/', getGroups);
-router.get('/:groupId', getGroupsById);
-router.patch('/update/:groupId', updateGroup);
+router.post("/", createGroup);
+router.delete("/delete/:groupId", deleteGroup);
+router.get("/", getGroups);
+router.get("/:groupId", getGroupsById);
+router.patch("/update/:groupId", updateGroup);
 
 module.exports = router;
