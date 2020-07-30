ba52e343bd0abccefa4ca3426792af1988f400fc
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-07-30 17:41:11 +0300

    CreateEditGroupModal tests

--- frontend/src/components/tests/CreateEditGroupModal.test.js
+++ frontend/src/components/tests/CreateEditGroupModal.test.js
@@ -1,15 +1,52 @@
 import React from "react";
 import { shallow, configure } from "enzyme";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
 import CreateEditGroupModal from "../CreateEditGroupModal";
 import Adapter from "enzyme-adapter-react-16";
-import { Provider } from "react-redux";
-import { store } from "../../redux/store";
+import configureStore from "redux-mock-store";
+import TestRenderer from "react-test-renderer";
+import ReactDOM from "react-dom";
+
+const mockStore = configureStore([]);
 
 configure({ adapter: new Adapter() });
 
 describe("CreateEditGroupModal", () => {
-  it("renders create", () => {
-    const modal = shallow(<CreateEditGroupModal />);
-    expect(modal.find("Button").length).toEqual(1);
+  let store;
+  let component;
+
+  beforeAll(() => {
+    ReactDOM.createPortal = jest.fn((element, node) => {
+      return element;
+    });
+  });
+  afterEach(() => {
+    ReactDOM.createPortal.mockClear();
+  });
+  beforeEach(() => {
+    store = mockStore({
+      id: "3"
+    });
+    component = renderer.create(
+      <Provider store={store}>
+        <CreateEditGroupModal />
+      </Provider>
+    );
+  });
+
+  it("render a button to pen modal", () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("render fields empty without given state -> create", () => {
+    renderer.act(() => {
+      console.log(component.root.findByType("button").props);
+      component.root.findByType("button").props.onClick();
+    });
+  });
+
+  it("render fields with given state from store -> edit", () => {
+    expect(true).toEqual(true);
   });
 });
