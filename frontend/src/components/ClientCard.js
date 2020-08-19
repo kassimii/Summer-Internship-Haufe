3956bb9be750dd92e51592c72deed415425535ae
turdasanbogdan <bogdanturdasan@bogdans-mbp.home>
2020-08-19 11:40:32 +0300

    get Client added

--- frontend/src/components/ClientCard.js
+++ frontend/src/components/ClientCard.js
@@ -1,15 +1,19 @@
 import React from "react";
+import ClientUpdateModal from "./ClientUpdateModal";
 
 const ClientCard = (props) => {
+
+  console.log(props.client.id);
   return (
     <div className="card">
       <img src="" className="card-img-top" alt="" />
       <div className="card-body">
-        <h5 className="card-title">Client title {props.id}</h5>
+        <h5 className="card-title">{props.client.name}</h5>
         <p className="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
+        <ClientUpdateModal id = {props.client.id} />
         <a href="/" className="btn btn-primary">
           View Details
         </a>
